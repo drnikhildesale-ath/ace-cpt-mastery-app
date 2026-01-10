e020a0c98eaa315481680c6c69fb3a0694fcb36f
drnikhildesale-ath <drnikhildesale@gmail.com>
2026-01-10 16:00:03 +0530

    Fix: Remove duplicate App function declaration

--- src/App.jsx
+++ src/App.jsx
@@ -26,14 +26,4 @@ function App() {
 }
 
 export default App;
-function App() {
-  return (
-    <div style={{ padding: '20px', textAlign: 'center' }}>
-      <h1>ACE-CPT Mastery Platform</h1>
-      <p>Clinical fitness certification course with AI-powered quiz generation</p>
-      <p style={{ color: '#666', marginTop: '30px' }}>Welcome to your ACE-CPT Mastery platform!</p>
-    </div>
-  );
-}
 
-export default App;
